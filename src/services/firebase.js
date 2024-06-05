4be651a710d3031828242b38f006dc5cc84575de
Ebuka-Francis <nwokefrancis99@gmail.com>
2024-06-05 15:39:55 +0100

    same

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,18 @@
-import firebase from "firebase/app";
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+// import firebase from "firebase/app";
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// import 'firebase/storage';
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,   
-}
+// const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,   
+// }
 
-export default function initFirebase() {
-    console.log(firebaseConfig)
-    if(!firebase.apps.length){
-        firebase.initializeApp(firebaseConfig);
-    }
-}
+// export default function initFirebase() {
+//     console.log(firebaseConfig)
+//     if(!firebase.apps.length){
+//         firebase.initializeApp(firebaseConfig);
+//     }
+// }
