f0013d9e9b6358b72edf12c3fc7868a0d890f96f
Ebuka-Francis <nwokefrancis99@gmail.com>
2024-07-18 16:45:43 +0100

    new add

--- src/app/(formauth)/signup/page.jsx
+++ src/app/(formauth)/signup/page.jsx
@@ -3,7 +3,7 @@ import SignupPage from '@/screens/signup';
 
 export default function page() {
   return (
-    <div style={{paddingInline: '13px'}}>
+    <div style={{paddingInline: '20px'}}>
        <SignupPage />
     </div>
   )
